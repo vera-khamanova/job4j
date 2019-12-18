17e68267021c941581957706742ec5e816f559a3
vera-khamanova <mona81lj@gmail.com>
2019-12-18 21:15:33 +0200

    Calculate

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+public class Calculate {
+    public static void add(double first, double second) {
+        double result = first + second;
+        System.out.println(first + " + " + second + " = " + result);
+    }
+    public static void div(double first, double second) {
+        double result = first / second;
+        System.out.println(first + " / " + second + " = " + result);
+    }
+    public static void multiply(double first, double second) {
+        double result = first * second;
+        System.out.println(first + " * " + second + " = " + result);
+    }
+    public static void subtrack(double first, double second) {
+        double result = first - second;
+        System.out.println(first + " - " + second + " = " + result);
+    }
+
+    public static void main(String[] args) {
+        add(1, 1);
+        div(4, 2);
+        multiply(2, 1);
+        subtrack(10, 5);
+    }
+}
