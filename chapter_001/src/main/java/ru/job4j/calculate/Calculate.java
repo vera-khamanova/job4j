bc9754929881ca441936c2533929355457700c03
vera-khamanova <mona81lj@gmail.com>
2019-12-18 21:29:54 +0200

    5.8. JavaDoc. Code convention[#197026]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,23 +1,61 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Calculate решение задачи части 001 урок 1
+ * @author verahamanova
+ * since 05.09.2019
+ */
+
 public class Calculate {
+
+    /**
+     * Сложение
+     * @param first первый аргумент
+     * @param second второй аргумент
+     * @return результат
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
+
+    /**
+     * Деление
+     * @param first Первый аргумент
+     * @param second Второй аргумент
+     * @return результат
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + " / " + second + " = " + result);
     }
+
+    /**
+     * Умножение
+     * @param first Первый аргумент
+     * @param second Второй аргумент
+     * @return результат
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
+
+    /**
+     * Вычитание
+     * @param first Первый аргумент
+     * @param second Второй аргумент
+     * @return результат
+     */
     public static void subtrack(double first, double second) {
         double result = first - second;
         System.out.println(first + " - " + second + " = " + result);
     }
 
+    /**
+     * Конструктор, вывод строк в консоль
+     * @param args - arg.
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Vera Khamanova (mona81@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
