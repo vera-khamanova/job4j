e86d67caa5505f7722f63559fbcf0aae6771902e
vera-khamanova <mona81lj@gmail.com>
2020-01-20 14:38:14 +0200

    6. Видео.[#197042]

---
+++ chapter_001/src/main/java/ru/job4j/ArrayDefinition.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class ArrayDefinition {
+
+    public static void main (String[] args){
+        short[] ages = new short[10];
+        String[] names = new String[100500];
+        float[] prices = new float[40];
+    }
+
+
+
+}
