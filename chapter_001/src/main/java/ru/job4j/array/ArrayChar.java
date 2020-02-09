ddfd7210f02f924c7829558b5f5f50de7038bc67
vera-khamanova <mona81lj@gmail.com>
2020-02-09 23:07:36 +0200

    6.4. Слово начинается с ...[#197037]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,18 @@
+package ru.job4j.array;
+
+public class ArrayChar {
+
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = false;
+        for(int index = 0; index<pref.length; index++){
+           if (pref[index] != word[index] ) {
+               result = false;
+           }
+           else{
+               result = true;
+                }
+
+        }
+        return result;
+    }
+}
