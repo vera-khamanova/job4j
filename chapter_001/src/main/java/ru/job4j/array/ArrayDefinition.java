d5bc92ac27a1a743266f645cfedbc9529789c951
vera-khamanova <mona81lj@gmail.com>
2020-02-12 14:57:54 +0200

    6.0.2. Заполнение массива. [#226300]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+
+    public static void main (String[] args){
+        short[] ages = new short[10];
+        float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Vera";
+        names[1] = "Masha";
+        names[2] = "Lana";
+        names[3] = "Sveta";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
+    }
+
+
+
+
+}
